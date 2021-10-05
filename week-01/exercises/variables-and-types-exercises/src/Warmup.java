089d6a09793f31d281eebef4848088eaf8dc3813
James Hall <JHall@dev-10.com>
2021-10-05 14:27:25 -0500

    something did not belong within this list

--- week-01/exercises/variables-and-types-exercises/src/Warmup.java
+++
@@ -1,8 +0,0 @@
-public class Warmup {
-    public static void main(String[] args) {
-        int dog = 5;
-        System.out.println("There are " + dog);
-        dog++;
-        System.out.println("There are " + dog);
-    }
-}
